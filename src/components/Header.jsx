2b9fce3981c95e9b4b13ff013ce6b74c38dbe26d
Samael Marjan <samaaelmarjan@gmail.com>
2022-11-10 15:55:48 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='block'>
+      <div>
+        <a href='#/'>Shopping Cart</a>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='col-2'>Main</div>
+  )
+}
+
+export default Main
